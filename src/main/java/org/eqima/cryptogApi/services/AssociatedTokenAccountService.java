02cfa93a91217a3f728c2602fc8489935343b219
haritsimba <haritsimbafranck@gmail.com>
2025-03-23 23:47:58 +0300

    modif : associated token account (service, entite)

--- src/main/java/org/eqima/cryptogApi/services/AssociatedTokenAccountService.java
+++ src/main/java/org/eqima/cryptogApi/services/AssociatedTokenAccountService.java
@@ -1,8 +1,16 @@
 package org.eqima.cryptogApi.services;
 
+import org.eqima.cryptogApi.entities.AssociatedTokenAccount;
+import org.eqima.cryptogApi.repositories.AssociatedTokenAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AssociatedTokenAccountService {
+    @Autowired
+    AssociatedTokenAccountRepository associatedTokenAccountRepository;
 
+    public AssociatedTokenAccount saveAssociatedTokenAccount(AssociatedTokenAccount associatedTokenAccount){
+        return associatedTokenAccountRepository.save(associatedTokenAccount);
+    }
 }
